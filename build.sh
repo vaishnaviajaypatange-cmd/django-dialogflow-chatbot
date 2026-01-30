41af2ed7ec020c1f0501c13f3895ccde8727e033
vaishnavipatange55 <vaishnavipatange01@gmail.com>
2026-01-30 17:22:47 +0530

    Fix WhiteNoise middleware order

---
+++ build.sh
@@ -0,0 +1,5 @@
+
+```bash
+#!/usr/bin/env bash
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
--- it_chatbot/settings.py
+++ it_chatbot/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'it_chatbot.urls'
@@ -117,11 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-
+    BASE_DIR / "chat" / "static",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
